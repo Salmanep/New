eafd0a4f621ca06ef14503eca3407c4d4f96691c
Salmanep <salmanulfaris2040@gmail.com>
2021-04-27 22:50:04 +0530

    link added

--- Myapp/urls.py
+++ Myapp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('link', views.link, name='link'),
 ]
--- Myapp/views.py
+++ Myapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
+def link(request):
+    return render(request, 'link.html')
